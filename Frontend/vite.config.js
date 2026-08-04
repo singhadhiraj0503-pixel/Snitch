af6517179a2438b4e2399c240c55b76a686241ce
singhadhiraj0503-pixel <singhadhiraj0503@gmail.com>
2026-08-05 00:14:28 +0530

    update api url

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,9 +8,11 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://snitch-yjz5.onrender.com",
+        // target: "http://localhost:3000",
         changeOrigin: true,
-        secure: false,},
+        secure: false,
+      },
     },
   },
 });
